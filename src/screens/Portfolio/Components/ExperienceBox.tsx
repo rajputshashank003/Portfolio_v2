699de3c67257bffab26bea722c1fa4bd1a00e395
rajputshashank003 <hariomshashank@gmail.com>
2025-12-17 01:00:20 +0530

    fix: imports

--- src/screens/Portfolio/Components/ExperienceBox.tsx
+++ src/screens/Portfolio/Components/ExperienceBox.tsx
@@ -88,8 +88,8 @@ const ExperienceBox = ({
                                     <div className='flex gap-[2px] justify-center items-center'>
                                         {opt.from} - {opt.to === 'I'
                                             ? <div className='text-zinc-500'>
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-infinity"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9.828 9.172a4 4 0 1 0 0 5.656a10 10 0 0 0 2.172 -2.828a10 10 0 0 1 2.172 -2.828a4 4 0 1 1 0 5.656a10 10 0 0 1 -2.172 -2.828a10 10 0 0 0 -2.172 -2.828" /></svg> 
-                                            </div> 
+                                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-infinity"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9.828 9.172a4 4 0 1 0 0 5.656a10 10 0 0 0 2.172 -2.828a10 10 0 0 1 2.172 -2.828a4 4 0 1 1 0 5.656a10 10 0 0 1 -2.172 -2.828a10 10 0 0 0 -2.172 -2.828" /></svg>
+                                            </div>
                                             : opt.to
                                         }
                                     </div>
@@ -121,7 +121,7 @@ const ExperienceBox = ({
                         && <div className='absolute right-[12px] top-[12px]'>
                             <HoverChip label='Project link'>
                                 <div className='h-full cursor-pointer text-zinc-500 flex justify-center items-center' onClick={() => navigate(opt.projectLink)}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
                                 </div>
                             </HoverChip>
                         </div>
--- src/screens/Portfolio/Components/ProjectBox.tsx
+++ src/screens/Portfolio/Components/ProjectBox.tsx
@@ -1,7 +1,8 @@
 import { useRef, useState } from "react";
 import gsap from "gsap";
-import { map, set } from "lodash";
+import { map } from "lodash";
 import { HoverChip } from "../../../components/HoverChip";
+import { useNavigate } from "react-router-dom";
 
 const ProjectBox = ({
     project,
@@ -20,6 +21,7 @@ const ProjectBox = ({
     techStack: string[];
     projectLink: string;
 }) => {
+    const navigate = useNavigate();
     const descRef = useRef<HTMLDivElement | null>(null);
     const [isExpanded, setIsExpanded] = useState(false);
 
@@ -75,10 +77,10 @@ const ProjectBox = ({
                             {duration}
                         </div>
                     </div>
-                    {projectLink 
+                    {projectLink
                         && <HoverChip label='Project link'>
-                            <div className='h-full cursor-pointer text-zinc-500 flex justify-center items-center' onClick={() => navigate(opt.projectLink)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
+                            <div className='h-full cursor-pointer text-zinc-500 flex justify-center items-center' onClick={() => navigate(projectLink)}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
                             </div>
                         </HoverChip>
                     }
