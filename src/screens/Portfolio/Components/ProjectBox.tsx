58c727550de9948506f7646032535576758eba31
rajputshashank003 <hariomshashank@gmail.com>
2026-01-31 01:56:09 +0530

    fix: product design

--- src/screens/Portfolio/Components/ProjectBox.tsx
+++ src/screens/Portfolio/Components/ProjectBox.tsx
@@ -77,13 +77,35 @@ const ProjectBox = ({
                             {duration}
                         </div>
                     </div>
-                    {projectLink
-                        && <HoverChip label='Project link'>
-                            <div className='h-full cursor-pointer text-zinc-500 flex justify-center items-center' onClick={() => navigate(projectLink)}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
-                            </div>
-                        </HoverChip>
-                    }
+                   <div className="flex flex-row gap-[12px] justify-center items-center">
+                        <div
+                            style={{
+                                transform: isExpanded ? 'rotate(0deg)' : 'rotate(90deg)',
+                            }}
+                            className="h-fit w-fit duration-400"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="18"
+                                height="18"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <polyline points="6 9 12 15 18 9" />
+                            </svg>
+                        </div>
+                        {projectLink
+                            && <HoverChip label='Project link'>
+                                <div className='h-full cursor-pointer text-zinc-500 flex justify-center items-center' onClick={() => navigate(projectLink)}>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-link"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M9 15l6 -6" /><path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464" /><path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463" /></svg>
+                                </div>
+                            </HoverChip>
+                        }
+                   </div>
                 </div>
             </div>
 
